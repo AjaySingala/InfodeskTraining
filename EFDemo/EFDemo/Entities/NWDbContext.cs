feccf270ee6088289efc0767209bf96468cc763b
AjaySingala <ajay.singala@gmail.com>
2023-06-30 14:07:33 +0530

    AD.NET and EF demos

---
+++ EFDemo/EFDemo/Entities/NWDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo.Entities
+{
+    public class NWDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(t => new { t.OrderId, t.ProductId });
+        }
+    }
+}
